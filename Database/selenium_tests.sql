e9c5383c51e8f27ce3a2a2763325bfc78f6280e2
Snigdha <Snigdha@LAPTOP-6EHU5QSS>
2024-10-20 14:43:10 +0530

    Uploading database .sql file

---
+++ Database/selenium_tests.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test_cases (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    test_case_name VARCHAR(255) NOT NULL,
+    result VARCHAR(50) NOT NULL,
+    execution_time DATETIME NOT NULL
+);
